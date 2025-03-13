b76e9be0b4f296e62ea09a9645574529cd696011
SHREYA KAMATH <shreyawillapply@gmail.com>
2025-03-13 19:19:24 -0400

    Changed slices

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,7 @@
 import "./App.css";
-import { Home } from "./pages/home";
 import { Provider } from "react-redux";
 import { store } from "./redux";
+import { Home } from "./pages/home";
 
 function App() {
     return (
