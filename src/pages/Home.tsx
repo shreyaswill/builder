8f0643361663f6e3ca0122ad67b0d2773c23cab2
SHREYA KAMATH <shreyawillapply@gmail.com>
2025-03-14 13:46:42 -0230

    no miss-redirections

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -1,8 +1,19 @@
+import { useEffect } from "react";
+import { useNavigate } from "react-router-dom";
 import { Canvas } from "../components/Canvas/Canvas";
 import { SideBar } from "../components/Sidebar/Sidebar";
 import "./Home.css";
 
 export const Home: React.FC = () => {
+    const navigate = useNavigate();
+
+    useEffect(() => {
+        // Redirect to login if no username is found in localStorage
+        if (!localStorage.getItem("username")) {
+            navigate("/");
+        }
+    }, [navigate]);
+
     return (
         <div className="container">
             <SideBar />
