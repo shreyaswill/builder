a302a7e79d75756fbde86cc00196e449a3e521a7
SHREYA KAMATH <shreyawillapply@gmail.com>
2025-03-15 19:07:12 -0230

    added tooltip

--- src/components/AddElement/AddElement.tsx
+++ src/components/AddElement/AddElement.tsx
@@ -8,33 +8,39 @@ import { DispatchType, RootState } from "../../redux";
 //
 
 export const AddElement: React.FC = () => {
-    const selected = useSelector((state: RootState) => getSelectedIds(state))!;
-    const dispatch = useDispatch<DispatchType>();
-    const addDiv = (pid?:number) => {
-        dispatch(addElement({ pid, id: selected.nextId }));
-        dispatch(incrementId());
-    };
-    const [propObject, dragRef] = useDrag(() => {
-        return {
-            type: DraggableTypes.DIV_ELEMENT,
-            item: {name: DraggableTypes.DIV_ELEMENT},
-            end: (item, monitor) => {
-                const resultFromDrop = monitor.getDropResult<{id: number | undefined}>();
-                if (item && resultFromDrop) {
-                    //dropped new component into parent (resultFromDrop.id) else Canvas
-                    addDiv(resultFromDrop.id);
-                }
-            },
-            collect: (monitor) => {
+        const selected = useSelector((state: RootState) => getSelectedIds(state))!;
+        const dispatch = useDispatch<DispatchType>();
+        const addDiv = (pid?: number) => {
+                dispatch(addElement({ pid, id: selected.nextId }));
+                dispatch(incrementId());
+        };
+        const [propObject, dragRef] = useDrag(() => {
                 return {
-                    opacity: monitor.isDragging() ? 0.5 : 1,
-                };
-            },
-        }
-    }, [selected.nextId]);
-    return (
-        <button ref={dragRef as unknown as React.RefObject<HTMLButtonElement>} className="add-div-btn" onClick={() => addDiv(selected.selectedId)} style={{ opacity: propObject.opacity }}>
-           Add Div
-        </button>
-    );
+                        type: DraggableTypes.DIV_ELEMENT,
+                        item: { name: DraggableTypes.DIV_ELEMENT },
+                        end: (item, monitor) => {
+                                const resultFromDrop = monitor.getDropResult<{ id: number | undefined }>();
+                                if (item && resultFromDrop) {
+                                        //dropped new component into parent (resultFromDrop.id) else Canvas
+                                        addDiv(resultFromDrop.id);
+                                }
+                        },
+                        collect: (monitor) => {
+                                return {
+                                        opacity: monitor.isDragging() ? 0.5 : 1,
+                                };
+                        },
+                }
+        }, [selected.nextId]);
+        return (
+                <button
+                        ref={dragRef as unknown as React.RefObject<HTMLButtonElement>}
+                        className="add-div-btn tooltip"
+                        onClick={() => addDiv(selected.selectedId)}
+                        style={{ opacity: propObject.opacity }}
+                >
+                        Add Div
+                        <span className="tooltip-text">Drag to add a new div</span>
+                </button>
+        );
 };
